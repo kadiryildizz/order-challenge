1e579d2d9bcf3724f9b501fc646e8b4ec412b793
Kadir Yıldız <kadiryildiz@hotmail.com.tr>
2022-09-26 00:39:20 +0300

    install-sh-updated

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-composer install
 cp .env.example .env
+composer install
 ./vendor/bin/sail up -d
+sleep 20
 docker exec -it order-challenge-laravel.test-1 /var/www/html/artisan key:generate
 docker exec -it order-challenge-laravel.test-1 /var/www/html/artisan migrate:refresh --seed
 docker exec -it order-challenge-laravel.test-1 /var/www/html/artisan passport:install
